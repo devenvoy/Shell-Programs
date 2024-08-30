66abb1d16ca818cdccee74b2db9440dc2e2b28b4
devenvoy <iambeast0069@gmail.com>
2024-08-31 00:40:44 +0530

    minor name change

---
+++ Journal/draw_pattern2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# draw pattern
+<< comment
+*
+**
+***
+****
+*****
+comment
+
+for i in {1..5}
+do
+    for (( j = 1 ; j <= i ; j++))
+    do
+        echo -n "*"
+    done
+    echo ""
+done
---
+++ Journal/prime_nums3.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+
+#2. Write a shell script to find all prime numbers in given range.
